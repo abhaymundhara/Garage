e06e30ce4e84be8546a4ade396c1273b00196329
Abhay Mundhara <abhay.mundhara@gmail.com>
2024-04-02 11:35:25 +0100

    update connection

--- connection.php
+++ connection.php
@@ -3,9 +3,9 @@
 function Connect()
 {
 	$dbhost = "localhost";
-	$dbuser = "u212206598_Garage";
+	$dbuser = "u212206598_abhay";
 	$dbpass = "Abhay@0811";
-	$dbname = "u212206598_abhay";
+	$dbname = "u212206598_Garage";
 
 	//Create Connection
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
