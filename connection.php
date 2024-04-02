d25d8787f486f23d628be84b9c73dc8ee8a42dee
Abhay Mundhara <abhay.mundhara@gmail.com>
2024-04-02 11:32:46 +0100

    Update remote connection cred.

--- connection.php
+++ connection.php
@@ -3,9 +3,9 @@
 function Connect()
 {
 	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "root";
-	$dbname = "Garage";
+	$dbuser = "u212206598_Garage";
+	$dbpass = "Abhay@0811";
+	$dbname = "u212206598_abhay";
 
 	//Create Connection
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
