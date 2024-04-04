5b5007f512b183410f73a83cf1aaf4f3d92a7fd3
Abhay Mundhara <abhay.mundhara@gmail.com>
2024-04-04 06:53:00 +0100

    fine&toll

--- employeeview.php
+++ employeeview.php
@@ -1,68 +1,80 @@
 <!DOCTYPE html>
 <html>
 <?php 
-session_start();
-require 'connection.php';
-$conn = Connect();
+ include('session_employee.php');
+if(!isset($_SESSION['login_employee'])){
+    session_destroy();
+    header("location: employeelogin.php");
+}
 ?>
+<title>Update Car </title>
+
 <head>
-<link rel="shortcut icon" type="image/png" href="assets/img/P.png.png">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-<link rel="stylesheet" href="assets/w3css/w3.css">
-<link rel="stylesheet" type="text/css" href="assets/css/customerlogin.css">
-<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" media="screen" href="assets/css/clientpage.css" />
+    <script type="text/javascript" src="assets/ajs/angular.min.js"> </script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="shortcut icon" type="image/png" href="assets/img/P.png.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/w3css/w3.css">
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/clientpage.css" />
 </head>
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-<!-- Navigation -->
+
+<body ng-app="">
+
+
+    <!-- Navigation -->
+    <!-- Navigation -->
     <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="color: black">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                     <i class="fa fa-bars"></i>
-                    </button>
+                </button>
                 <a class="navbar-brand page-scroll" href="index.php">
-                   Car Rentals </a>
+                    Car Rentals </a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
 
             <?php
                 if(isset($_SESSION['login_employee'])){
-            ?> 
+            ?>
             <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
                 <ul class="nav navbar-nav">
                     <li>
                         <a href="index.php">Home</a>
                     </li>
                     <li>
-                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_employee']; ?></a>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome
+                            <?php echo $_SESSION['login_employee']; ?></a>
                     </li>
                     <li>
-                    <ul class="nav navbar-nav navbar-right">
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Control Panel <span class="caret"></span> </a>
-                <ul class="dropdown-menu">
-              <li> <a href="entercar.php">Add Car</a></li>
-              <li> <a href="entercustomer.php"> Add Customer</a></li>
-              <li> <a href="employeeview.php">View</a></li>
-              <li> <a href="prereturncar.php">Return Now</a></li>
-              <li> <a href="mybookings.php"> My Bookings</a></li>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button"
+                                    aria-haspopup="true" aria-expanded="false"><span
+                                        class="glyphicon glyphicon-user"></span> Control Panel <span
+                                        class="caret"></span> </a>
+                                <ul class="dropdown-menu">
+                                    <li> <a href="entercar.php">Add Car</a></li>
+                                    <li> <a href="entercustomer.php"> Add Customer</a></li>
+                                    <li> <a href="employeeview.php">View</a></li>
+                                    <li> <a href="prereturncar.php">Return Now</a></li>
+                                    <li> <a href="mybookings.php"> My Bookings</a></li>
 
-            </ul>
-            </li>
-          </ul>
+                                </ul>
+                            </li>
+                        </ul>
                     </li>
                     <li>
                         <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
                     </li>
                 </ul>
             </div>
-            
+
             <?php
                 }
-                
                 else {
             ?>
 
@@ -75,79 +87,109 @@ $conn = Connect();
                         <a href="employeelogin.php">Employee</a>
                     </li>
                     <li>
-                        <a href="admin/index.php">Admin Panel</a>
+                        <a href="admin/index.php"> Admin Panel </a>
                     </li>
                 </ul>
             </div>
-                <?php   }
+            <?php   }
                 ?>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
- 
-<?php $login_employee = $_SESSION['login_employee']; 
 
-    $sql1 = "SELECT * FROM rentedcars rc, customer c, cars WHERE  c.customer_username = rc.customer_username AND rc.car_id = cars.car_id AND rc.return_status = 'NR'";
 
-    $result1 = $conn->query($sql1);
+    <div class="container" style="margin-top: 65px;">
+        <div class="col-md-7" style="float: none; margin: 0 auto;">
+            <div class="form-area">
+                <form role="form" action="process_fine&toll.php" method="POST">
+                    <br style="clear: both">
+                    <br>
 
-    if (mysqli_num_rows($result1) > 0) {
-?>
-<div class="container">
-      <div class="jumbotron">
-        <h1 class="text-center">Your Bookings</h1>
-        <p class="text-center"> Hope you enjoyed our service </p>
-      </div>
+                    <?php
+        $car_id = $_GET["id"];
+        $sql1 = "SELECT * FROM cars WHERE car_id = '$car_id'";
+        $result1 = mysqli_query($conn, $sql1);
+
+        if(mysqli_num_rows($result1)){
+            while($row1 = mysqli_fetch_assoc($result1)){
+                $car_name = $row1["car_name"];
+                $car_nameplate = $row1["car_nameplate"];
+                
+            }
+        }
+
+        ?>
+
+                    <h3 style="margin-bottom: 25px; text-align: center; font-size: 30px;"> UPDATE Car
+                        Details. </h3>
+
+                    <label>
+                        <h5>Selected Car: <b><?php echo($car_name);?></b></h5>
+                    </label>
+                    &nbsp;
+
+
+
+                    <label>
+                        <h5>Car NumberPlate: <b><?php echo($car_nameplate);?></b></h5>
+                    </label>
+
+                    <label for="fine_amount">
+                        <h5>Fine Amount:
+                    </label>
+                    <input type="number" id="fine_amount" name="fine_amount"><br><br>
+
+                    <label>
+                        <h5>Fine Date:</h5>
+                    </label>
+                    <input type="date" id="fine_date" name="fine_date"><br><br>
+
+
+                    <label for="fine_description">Fine Description:</label>
+                    <textarea id="fine_description" name="fine_description" rows="3" cols="50"
+                        required></textarea><br><br>
+
+                    <label for="toll_charge">Toll Charge:</label>
+                    <input type="number" id="toll_charge" name="toll_charge"><br><br>
+
+                    <label>
+                        <h5>Toll Date:</h5>
+                    </label>
+                    <input type="date" id="toll_date" name="toll_date"><br><br>
+
+
+                    <label for="toll_description">Toll Description:</label>
+                    <textarea id="toll_description" name="toll_description" rows="3" cols="50"
+                        required></textarea><br><br>
+
+
+                    </h5>
+
+                    <div class="form-group">
+                        <label>
+                            <h5>Upload picture(if any):</h5> &nbsp;
+                                <input name="uploadedimage" type="file">
+                        </label>
+                    </div>
+                    <input type="hidden" name="hidden_carid" value="<?php echo $car_id; ?>">
+                    <button type="submit" id="submit" name="submit" class="btn btn-success pull-right"> Submit
+                        for Rental</button>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div class="table-responsive" style="padding-left: 100px; padding-right: 100px;" >
-<table class="table table-striped">
-  <thead class="thead-dark">
-<tr>
-<th width="20%">Car</th>
-<th width="15%">Customer Name</th>
-<th width="20%">Rent Start Date</th>
-<th width="20%">Rent End Date</th>
-<th width="10%">Return Status</th>
-<th width="15%">Total Amount</th>
-</tr>
-</thead>
-<?php
-        while($row = mysqli_fetch_assoc($result1)) {
-?>
-<tr>
-<td><?php echo $row["car_name"]; ?></td>
-<td><?php echo $row["customer_username"]; ?></td>
-<td><?php echo $row["rent_start_date"] ?></td>
-<td><?php echo $row["rent_end_date"]; ?></td>
-<td><?php echo $row["return_status"]; ?></td>
-<td>Rs. <?php echo $row["total_amount"]; ?></td>
-</tr>
-<?php        } ?>
-                </table>
-                </div> 
-        <?php } else {
-            ?>
-        <div class="container">
-      <div class="jumbotron">
-        <h1>No booked cars</h1>
-        <p> Rent some cars now <?php echo $conn->error; ?> </p>
-      </div>
-    </div>
-
-            <?php
-        } ?>   
 
 </body>
 <footer class="site-footer">
-        <div class="container">
-            <hr>
-            <div class="row">
-                <div class="col-sm-6">
-                    <h5>© <?php echo date("Y"); ?> Car Rentals</h5>
-                </div>
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-sm-6">
+                <h5>© <?php echo date("Y"); ?> Car Rentals</h5>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
+
 </html>
---
+++ process_fine&toll.php
@@ -0,0 +1,206 @@
+<html>
+
+<head>
+    <title> Customer Signup | Car Rentals </title>
+</head>
+<?php session_start(); 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<link rel="shortcut icon" type="image/png" href="assets/img/P.png.png">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/customerlogin.css">
+
+<link rel="stylesheet" href="assets/w3css/w3.css">
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="color: black">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="index.php">
+                    Car Rentals </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
+            <?php
+                if(isset($_SESSION['login_employee'])){
+            ?>
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome
+                            <?php echo $_SESSION['login_employee']; ?></a>
+                    </li>
+                    <li>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button"
+                                    aria-haspopup="true" aria-expanded="false"><span
+                                        class="glyphicon glyphicon-user"></span> Control Panel <span
+                                        class="caret"></span> </a>
+                                <ul class="dropdown-menu">
+                                    <li> <a href="entercar.php">Add Car</a></li>
+                                    <li> <a href="entercustomer.php"> Add Customer</a></li>
+                                    <li> <a href="employeeview.php">View</a></li>
+                                    <li> <a href="prereturncar.php">Return Now</a></li>
+                                    <li> <a href="mybookings.php"> My Bookings</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+
+            <?php
+                }
+
+                else {
+            ?>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="employeelogin.php">Employee</a>
+                    </li>
+                    <li>
+                        <a href="admin/index.php">Admin Panel</a>
+                    </li>
+
+                </ul>
+            </div>
+            <?php   }
+                ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <?php
+
+require 'connection.php';
+$conn = Connect();
+
+function GetImageExtension($imagetype) {
+    if(empty($imagetype)) return false;
+    
+    switch($imagetype) {
+        case 'assets/img/cars/bmp': return '.bmp';
+        case 'assets/img/cars/gif': return '.gif';
+        case 'assets/img/cars/jpeg': return '.jpg';
+        case 'assets/img/cars/png': return '.png';
+        default: return false;
+    }
+}
+
+$car_id = $conn->real_escape_string($_POST['hidden_carid']);
+$sql0 = "SELECT c.car_name, c.car_nameplate FROM cars c WHERE c.car_id = $car_id";
+$result0 = $conn->query($sql0);
+
+if(mysqli_num_rows($result0) > 0) {
+    while($row0 = mysqli_fetch_assoc($result0)){
+            $car_nameplate = $row0["car_nameplate"];
+    }
+}
+
+
+$fine_amount = $conn->real_escape_string($_POST['fine_amount']);
+$fine_desc = $conn->real_escape_string($_POST['fine_description']);
+$fine_date = date('Y-m-d', strtotime($_POST['fine_date']));
+$toll_amount = $conn->real_escape_string($_POST['toll_charge']);
+$toll_desc = $conn->real_escape_string($_POST['toll_description']);
+$toll_date = date('Y-m-d', strtotime($_POST['toll_date']));
+$car_availability = "yes";
+$success = "";
+
+
+// $query = "INSERT into cars(car_name,car_nameplate,ac_price,non_ac_price,car_availability) VALUES('" . $car_name . "','" . $car_nameplate . "','" . $ac_price . "','" . $non_ac_price . "','" . $car_availability ."')";
+// $success = $conn->query($query);
+
+
+if (!empty($_FILES["uploadedimage"]["name"])) {
+    $file_name=$_FILES["uploadedimage"]["name"];
+    $temp_name=$_FILES["uploadedimage"]["tmp_name"];
+    $imgtype=$_FILES["uploadedimage"]["type"];
+    $ext= GetImageExtension($imgtype);
+    $imagename=$_FILES["uploadedimage"]["name"];
+    $target_path = "assets/img/cars/".$imagename;
+
+    if(move_uploaded_file($temp_name, $target_path)) {
+        //$query0="INSERT into cars (car_img) VALUES ('".$target_path."')";
+      //  $query0 = "UPDATE cars SET car_img = '$target_path' WHERE ";
+        //$success0 = $conn->query($query0);
+
+        $query = "INSERT into fines_and_tolls(car_id,car_name,car_nameplate,fine_amount,fine_date,fine_description,toll_charge,toll_date,toll_description,car_img) VALUES('" . $car_id . "','" . $car_name . "','" . $car_nameplate . "','" . $fine_amount . "','" . $fine_date . "','" . $fine_desc . "','" . $toll_amount . "','" . $toll_date . "','" . $toll_desc . "','". $target_path ."')";
+        
+        $success = $conn->query($query);
+
+        
+    } 
+
+}
+else
+$query = "INSERT into fines_and_tolls(car_id,car_nameplate,fine_amount,fine_date,fine_description,toll_charge,toll_date,toll_description) VALUES('" . $car_id . "','" . $car_nameplate . "','" . $fine_amount . "','" . $fine_date . "','" . $fine_desc . "','" . $toll_amount . "','" . $toll_date . "','" . $toll_desc . "')";
+        
+$success = $conn->query($query);
+
+
+if (!$success){ ?>
+
+    <div class="container">
+        <div class="jumbotron" style="text-align: center;">
+            Car with the same vehicle number already exists!
+            <?php 
+            echo $conn->error; 
+            echo $fine_amount;
+            echo $fine_date;
+            ?>
+            <br><br>
+            <a href="fine&toll.php" class="btn btn-default"> Go Back </a>
+        </div>
+        <?php	
+}
+else {
+    ?>
+    <div class = container>
+    <div class="jumbotron" style="text-align: center;">
+            Car added successfully!
+            <br><br>
+            <a href="fine&toll.php" class="btn btn-default"> Go Back </a>
+        </div></div>
+    <?php
+    //header("location: entercar.php"); //Redirecting 
+}
+
+$conn->close();
+
+?>
+
+</body>
+<footer class="site-footer">
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-sm-6">
+                <h5>© <?php echo date("Y"); ?> Car Rentals</h5>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</html>
